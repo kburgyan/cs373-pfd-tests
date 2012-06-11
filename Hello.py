eab95d53d7c7c261deb399445bdb69f817eb1268
kburgyan <kburgyan@utexas.edu>
2012-06-10 22:49:27 -0500

    the initial file

---
+++ Hello.py
@@ -0,0 +1,4 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+print "Nothing to be done. blah blah blah"
---
+++ RunCollatz.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# ------------------------------
+# projects/collatz/RunCollatz.py
+# Copyright (C) 2011
+# Glenn P. Downing
+# -------------------------------
+
+"""
+To run the program
+% python RunCollatz.py < RunCollatz.in > RunCollatz.out
+% chmod ugo+x RunCollatz.py
+% RunCollatz.py < RunCollatz.in > RunCollatz.out
+
+To document the program
+% pydoc -w Collatz
+"""
+
+# -------
+# imports
+# -------
+
+import sys
+
+from Collatz import collatz_solve
+
+# ----
+# main
+# ----
+
+collatz_solve(sys.stdin, sys.stdout)
